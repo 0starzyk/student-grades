e34a63876d36502e19536e9f2be3d1d5ccbe18ad
0starzyk <starzyk@student.agh.edu.pl>
2023-03-27 17:18:13 +0200

    Limitation of significant features

--- main.py
+++ main.py
@@ -14,16 +14,15 @@ def plot_linear(x, y, title):
 data = pd.read_csv("student-mat.csv", sep=";")
 label_column = "G3"
 features = data.select_dtypes(include=["int64"]).drop([label_column], axis=1)
+constrained_features = data[["G1", "G2"]]
 feature_names = np.array(features.columns)
 label = data[label_column]
 
-X = np.array(features)
-y = np.array(label)
-
-X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.2)
 linear = linear_model.LinearRegression()
-linear.fit(X_train, y_train)
-print(linear.score(X_test, y_test))
 
-for index, feature in enumerate(feature_names):
-    plot_linear(X[:, index], y, feature)
+
+for X in [np.array(features), np.array(constrained_features)]:
+    y = np.array(label)
+    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.2)
+    linear.fit(X_train, y_train)
+    print(linear.score(X_test, y_test))
